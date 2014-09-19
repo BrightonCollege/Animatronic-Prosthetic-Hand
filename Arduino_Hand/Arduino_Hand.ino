4687a889476c040254354235fcf619e07b5536bd
Harry Beadle <harry.beadle@gmail.com>
2014-09-19 17:45:02 +0100

    Attached servo objects

--- Arduino_Hand/Arduino_Hand.ino
+++ Arduino_Hand/Arduino_Hand.ino
@@ -17,6 +17,13 @@ Servo Middle_Finger_Servo;
 Servo Ring_Finger_Servo;
 Servo Pinky_Finger_Servo;
 
+void setup() {
+    // Attach servos
+    Thumb_Finger_Servo.attach(3)
+    First_Finger_Servo.attach(5)
+    Middle_Finger_Serco.attach(6)
+    Ring_Finger_Servo.attach(10)
+    Pinky_Finger_Servo.attach(11)
 }
 
 void loop() {
