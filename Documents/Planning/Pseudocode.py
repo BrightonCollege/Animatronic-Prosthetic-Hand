886f084d2588ae384307104f3bfb6727d4003f71
Harry Beadle <harry.beadle@gmail.com>
2014-09-19 17:33:31 +0100

    Initial commit

---
+++ Documents/Planning/Pseudocode.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+def changePosition(Finger, Angle):
+    bluetoothSend(Channel)
+    bluetoothSend(Finger)
+    bluetoothSend(Angle)
+
+def doGesture(Gesture):
+    Gesture = Gesture.lower()
+    if Gesture == "rock":
+        changePosition(Thumb_Finger, 180)
+        changePosition(First_Finger, 180)
+        changePosition(Middle_Finger, 180)
+        changePosition(Ring_Finger, 180)
+        changePosition(Pinky_Finger, 180)
+    if Gesture == "paper":
+        changePosition(Thumb_Finger, 0)
+        changePosition(First_Finger, 0)
+        changePosition(Middle_Finger, 0)
+        changePosition(Ring_Finger, 0)
+        changePosition(Pinky_Finger, 0)
+    if Gesture == "scissors":
+        changePosition(Thumb_Finger, 180)
+        changePosition(First_Finger, 0)
+        changePosition(Middle_Finger, 0)
+        changePosition(Ring_Finger, 180)
+        changePosition(Pinky_Finger, 180)
