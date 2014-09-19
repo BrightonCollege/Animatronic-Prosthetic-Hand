446d0461ea445322461edbc789d2d4d909909e62
Harry Beadle <harry.beadle@gmail.com>
2014-09-19 17:44:53 +0100

    Added servo objects

--- Arduino_Hand/Arduino_Hand.ino
+++ Arduino_Hand/Arduino_Hand.ino
@@ -10,6 +10,12 @@
 // //
 
 void setup() {
+// Initalise Servo Objects
+Servo Thumb_Finger_Servo;
+Servo First_Finger_Servo;
+Servo Middle_Finger_Servo;
+Servo Ring_Finger_Servo;
+Servo Pinky_Finger_Servo;
 
 }
 
