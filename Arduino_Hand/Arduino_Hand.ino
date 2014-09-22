f8fa7baa8b243f94c4d7987860b484c523057923
Harry Beadle <harry.beadle@gmail.com>
2014-09-22 20:42:18 +0100

    Update

--- Arduino_Hand/Arduino_Hand.ino
+++ Arduino_Hand/Arduino_Hand.ino
@@ -11,66 +11,87 @@
 
 #include <Servo.h>
 
-// Initalise Servo Objects
+// Servo Setup
 Servo Thumb_Finger_Servo;
-Servo First_Finger_Servo;
+Servo Index_Finger_Servo;
 Servo Middle_Finger_Servo;
 Servo Ring_Finger_Servo;
 Servo Pinky_Finger_Servo;
 
-const int Baud = 9600;
-int FingerByte = 0;
-int AngleByte = 0;
 
-int Thumb_Finger = 0;
-int First_Finger = 1;
-int Middle_Finger = 2;
-int Ring_Finger = 3;
-int Pinky_Finger = 4;
+// Communication Constants and Variables
+const int Start_Byte = 83; // "S"
+const int Thumb_Finger_Byte = 48; // "0"
+const int Index_Finger_Byte = 49; // "1"
+const int Middle_Finger_Byte = 50; // "2"
+const int Ring_Finger_Byte = 51; // "3"
+const int Pinky_Finger_Byte = 52; // "4"
+const int Baud = 9600;
+int RX_Finger_Byte = 0;
+int RX_Angle_Byte = 0;
 
 void setup() {
-    // Attach servos
-    // Remember that Servos MUST be attched to pwm pins (marked with a ~)
-    Thumb_Finger_Servo.attach(3);
-    First_Finger_Servo.attach(5);
-    Middle_Finger_Servo.attach(6);
-    Ring_Finger_Servo.attach(10);
-    Pinky_Finger_Servo.attach(11);
-    // Setup Bluetooth serial line
-    Serial.begin(Baud); // Baud = 9600 bps
+  // Attach Servos
+  // Remember they can only be attached on PWM pins, marked with a "~"
+  Thumb_Finger_Servo.attach(3);
+  Index_Finger_Servo.attach(5);
+  Middle_Finger_Servo.attach(6);
+  Ring_Finger_Servo.attach(10);
+  Pinky_Finger_Servo.attach(11);
+  
+  // Setup Bluetooth serial line
+  Serial.begin(Baud); // Baud = 9600 bps
+  
+  // Initalise Servo Positions
+  Thumb_Finger_Servo.write(1);
+  Index_Finger_Servo.write(1);
+  Middle_Finger_Servo.write(1);
+  Ring_Finger_Servo.write(1);
+  Pinky_Finger_Servo.write(1);
 }
 
 void loop() {
-    // Only run if there is data in the buffer
-    if (Serial.available() > 0){ 
-        // Only run if there is the connection signal
-        if (Serial.read() == 255) {
-            FingerByte = Serial.read();
-            AngleByte = Serial.read();
-            // Correction
-            if (AngleByte == 0) {AngleByte = 1;}
-            if (AngleByte == 180) {AngleByte = 179;}
-            // Verification
-            if (0 <= FingerByte || FingerByte < 5) {
-                if (0 <= Angle || Angle < 180) {
-                    // Selection
-                    if (FingerByte == Thumb_Finger) {
-                        Thumb_Finger_Servo.write(AngleByte);
-                        
-                    } else if (FingerByte == First_Finger) {
-                        First_Finger_Servo.write(AngleByte);
-                        
-                    } else if (FingerByte == Middle_Finger) {
-                        Middle_Finger_Servo.write(AngleByte);
-                        
-                    } else if (FingerByte == Ring_Finger) {
-                        Ring_Finger_Servo.write(AngleByte);
-                        
-                    } else if (FingerByte == Pinky_Finger) {
-                        Pinky_Finger_Servo.write(AngleByte);
-                    }
-                }
-            }
-        }
+  // Only run if there is data in the buffer
+  if (Serial.available() > 0) {
+    // Only run if there is a connection signal
+    if (Serial.read() == Start_Byte) {
+      Serial.println("Message Receved");
+      delay(500);
+      // Read
+      RX_Finger_Byte = Serial.read();
+      delay(500);
+      RX_Angle_Byte = Serial.read();
+      Serial.println(RX_Finger_Byte);
+      Serial.println(RX_Angle_Byte);
+      Serial.println("Angle Byte in Degrees:");
+      Serial.println(Angle(RX_Angle_Byte));
+      // Verification
+      // Selection
+      if (RX_Finger_Byte == Thumb_Finger_Byte) {
+        Thumb_Finger_Servo.write(Angle(RX_Angle_Byte));
+        Serial.println("Thumb Moved");
+        
+      } else if (RX_Finger_Byte == Index_Finger_Byte) {
+        Index_Finger_Servo.write(Angle(RX_Angle_Byte));
+        Serial.println("Index Moved");
+        
+      } else if (RX_Finger_Byte == Middle_Finger_Byte) {
+        Middle_Finger_Servo.write(Angle(RX_Angle_Byte));
+        Serial.println("Middle Moved");
+        
+      } else if (RX_Finger_Byte == Ring_Finger_Byte) {
+        Ring_Finger_Servo.write(Angle(RX_Angle_Byte));
+        Serial.println("Ring Moved");
+        
+      } else if (RX_Finger_Byte == Pinky_Finger_Byte) {
+        Pinky_Finger_Servo.write(Angle(RX_Angle_Byte));
+        Serial.println("Pinky Moved");
+      }
     }
+  }
+}
+
+int Angle(int Byte) {
+  int Angle = map(Byte, 97, 122, 1, 179);
+  return Angle;
 }
