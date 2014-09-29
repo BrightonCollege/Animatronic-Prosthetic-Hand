458b1e8ef68139d0acbced9b40de094fa2b17856
Harry Beadle <harry.beadle@gmail.com>
2014-09-29 18:26:45 +0100

    Added bluetooth functionality, cleanup

--- Arduino_Hand/Arduino_Hand.ino
+++ Arduino_Hand/Arduino_Hand.ino
@@ -2,15 +2,10 @@
 
 // Animatonic Prosthetic Hand
 
-// //
-//
-// File to be transferred to the Arduino.
-// Collects Bluetooth input and outputs to the servos
-//
-// //
-
 #include <Servo.h>
-#define angle(ch) map(ch, 'a', 'z', 1, 165) // Angle adjustment required
+#include <SoftwareSerial.h>
+
+#define angle(ch) map(ch, 'a', 'z', 1, 165)
 
 #define DEBUG 1
 #define DEBUG_PRINT(msg)       \
@@ -23,75 +18,71 @@
 Servo fingers_servo[5];
 const char FINGERS_NAME[][7] = { "Thumb", "Index", "Middle", "Ring", "Pinky" };
 
-// Communication Constants
-const byte START_BYTE  = 'S';
-const int BAUD         = 9600;
+// Communication Setup
+const byte START_BYTE = 'S';
+int bluetoothTx = 11;
+int bluetoothRx = 12;
+SoftwareSerial bluetooth(bluetoothTx, bluetoothRx);
 
 void setup() {
   // Attach and Initalise Servos
-  const int PINS[] = { 3, 5, 6, 10, 11 };
+  const int PINS[] = { 3, 5, 6, 9, 10 };
   int i;
   for(i=0; i<5; i++) {
     fingers_servo[i].attach(PINS[i]);
     fingers_servo[i].write(1);
   }
-  // Setup Bluetooth serial line
-  Serial.begin(BAUD);
+  // Setup Bluetooth
+  bluetooth.begin(115200);
+  bluetooth.print("$");
+  bluetooth.print("$");
+  bluetooth.print("$");
+  delay(100);
+  bluetooth.println("U,9600,N"); // Set the baudrate to 9600
+  bluetooth.begin(9600);
 }
 
 void loop() {
   byte finger_byte = 0;
   byte angle_byte = 0;
   
-  while(Serial.read() != START_BYTE); // Wait for the start byte
+  while(bluetooth.read() != START_BYTE); // Wait for the start byte
 
   unsigned long start_time_ms = millis();
   unsigned long input_timeout_ms = 1000;
   
-  while(Serial.available() < 2) {
+  while(bluetooth.available() < 2) {
     if(millis() - start_time_ms >= input_timeout_ms) {
       // Timeout
-      DEBUG_PRINT("Timeout!");
-      Serial.write("t");
+      Serial.write("t"); // Write error message
       return;
     }
   }
-  
-  finger_byte = Serial.read();
 
   char input[3] = { 0, 0, 0 };
-  if(Serial.readBytes(input, 2) < 2) {
-    // TODO: handle input timeout error
+  if(bluetooth.readBytes(input, 2) < 2) {
+    // Buffer too small
+    bluetooth.write("b"); // Write error message
     return;
   }
+  
   finger_byte = input[0];
   angle_byte = input[1];
-
-  DEBUG_PRINT(finger_byte);
-  DEBUG_PRINT(angle_byte);
   
   int idx = finger_byte - '0'; // convert char to the int it represents
 
-  // '0'=thumb, '1'=index, ... , '4'=pinky
-
-  DEBUG_PRINT(idx);
   if(0 < idx || idx > 4) {
     // Finger is out of range
-    DEBUG_PRINT("finger_byte out of range!");
-    Serial.write("f");
+    bluetooth.write("f"); // Write error message
     return;
   }
 
   if(angle_byte < 'a' || angle_byte > 'z') {
     // Angle is out of range
-    DEBUG_PRINT("angle_byte out of range!");
-    Serial.write("a");
+    bluetooth.write("a"); // Write error message
     return;
   }
   
-  fingers_servo[idx].write(angle(angle_byte));
-  Serial.print(FINGERS_NAME[idx]);
-  Serial.print(" moved to ");
-  Serial.println(angle(angle_byte));
+  fingers_servo[idx].write(angle(angle_byte)); // Move Servo
 }
 
