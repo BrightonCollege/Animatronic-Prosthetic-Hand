ec4f3fdbecf0ca229f29f6660998318e03c2fe5f
Harry Beadle <harry.beadle@gmail.com>
2014-10-30 20:23:27 +0000

    changed nested if to switch

--- iOS_Hand/IOS_Hand/IOS_Hand/functions.playground/section-1.swift
+++ iOS_Hand/IOS_Hand/IOS_Hand/functions.playground/section-1.swift
@@ -10,38 +10,33 @@ func changePostition(Finger: Angle)
     bluetoothSend(Angle)
 }
 
-//fuction which enables user to interact playing game 'rock, paper, scissors'
+//fuction which enables user to send gestures
 func doGesture(Gesture)
 {
-    Gesture = Gesture.lowercaseString
+    switch Gesture.lowercaseString {
     
-    if Gesture == "rock"
-    {
+    case "rock":
         changePostition(Thumb_Finger, 180)
         changePostition(First_Finger, 180)
         changePostition(Middle_Finger,180)
         changePostition(Ring_Finger, 180)
         changePostition(Pinky_Finger, 180)
-    }
-    
-    if Gesture == "paper"
-    {
+    case "paper":
         changePostition(Thumb_finger, 0)
         changePostition(First_Finger, 0)
         changePostition(Middle_finger, 0)
         changePostition(Ring_Finger, 0)
         changePostition(Pinky_Finger, 0)
-    }
-    
-    if Gesture == "scissors"
-    {
+    case "scissors":
         changePostition(Thumb_Finger, 180)
         changePostition(First_Finger, 0)
         changePostition(Middle_Finger, 0)
         changePostition(Ring_Finger, 180)
         changePostition(Pinky_Finger, 180)
+    default:
+        //Handle Error
+        pass
     }
-    
 }
 
 //fuction which returns hand to inital position
